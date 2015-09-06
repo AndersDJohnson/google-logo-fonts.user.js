42ec850c424dd5c14ddbffb1f7fd2b61037d6d21
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-09-05 21:48:33 -0500

    Upgrade to jQuery 1.11.3 and https.

--- src/js/google-logo-fonts.user.js
+++ src/js/google-logo-fonts.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Google Logo Fonts
 // @namespace    https://github.com/AndersDJohnson/
-// @version      1.1.2
+// @version      1.1.3
 // @downloadURL  https://github.com/AndersDJohnson/google-logo-fonts.user.js/raw/master/src/js/google-logo-fonts.user.js
 // @updateURL    https://github.com/AndersDJohnson/google-logo-fonts.user.js/raw/master/src/js/google-logo-fonts.user.js
 // @description  Switch Google's logo to random fonts from Google Fonts API.
 // @author       Anders D. Johnson
 // @copyright    2015+, Anders D. Johnson
 // @match        https://www.google.com/*
-// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js
 // @require      https://raw.githubusercontent.com/typekit/webfontloader/master/webfontloader.js
 // @grant        GM_xmlhttpRequest 
 // ==/UserScript==
